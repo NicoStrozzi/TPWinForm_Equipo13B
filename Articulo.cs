d43ecf958e9bcef3aa2f86988a382c36c23980f2
nicostrozzi <nicostrozzi@hotmail.com>
2025-09-04 20:48:27 -0300

    Creacion primeras clases

---
+++ Articulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_GestionArticulos
+{
+    internal class Articulo
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string marca { get; set; }
+        public Categoria categoria { get; set; }
+        public float precio { get; set; }
+        public ImagenArticulo imagen { get; set; }
+        public int stock { get; set; }
+        public DateTime fechaAlta { get; set; }
+        public bool activo { get; set; }
+
+        
+
+    }
+}
---
+++ Marca.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_GestionArticulos
+{
+    internal class Marca
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descrpcion { get; set; }
+
+        public string paisOrigen { get; set; }
+        public string logoUrl { get; set; }
+    }
+}
