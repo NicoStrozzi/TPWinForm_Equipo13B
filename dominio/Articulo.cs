8b02e1dcebc3dc9bf111e1d97d741bb5deb686fb
nicostrozzi <nicostrozzi@hotmail.com>
2025-09-13 08:08:13 -0300

    Se agrego las tildes a la dvgArticulos

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,11 @@ namespace dominio
     public class Articulo
     {
         public int Id { get; set; }
+        [DisplayName("Código")]
         public string Codigo { get; set; }
+        [DisplayName("Número")]
         public string Nombre { get; set; }
+        [DisplayName("Descripción")]
         public string Descripcion { get; set; }
 
         public string Imagenes { get; set; }
